b350f6f64b7c9e545d6b339ca1ca3608c8562f33
Firdavs-22 <firdavsgaybullayev22@gmail.com>
2023-02-21 18:18:15 +0500

    add local storage

--- script/shop.js
+++ script/shop.js
@@ -1,51 +1,53 @@
 export default class Shop {
-    init(){
+    init() {
         this.products = undefined;
         this.cart = undefined;
     }
-    constructor(products,productDOM,cartDOM,totalDOM) {
+
+    constructor(products, productDOM, cartDOM, totalDOM) {
         this.init()
-        this.cart = new Cart(cartDOM,totalDOM)
-        this.products = new Products(products,productDOM,this.cart)
+        this.cart = new Cart(cartDOM, totalDOM)
+        this.products = new Products(products, productDOM, this.cart)
         this.render()
     }
 
-    render(){
+    render() {
         this.renderProducts();
         this.renderCart();
     }
 
-    renderProducts(){
+    renderProducts() {
         this.products.render()
     }
 
-    renderCart(){
-
+    renderCart() {
+        this.cart.render(this.products.products)
     }
 }
 
 class Products {
-    init(){
+    init() {
         this.products = []
         this.DOM = undefined
     }
-    constructor(products,DOM,Cart) {
+
+    constructor(products, DOM, Cart) {
         this.init()
         this.DOM = DOM
-        for (let i = 0,len = products.length; i < len; i++) {
-            this.products.push(new ProductItem(products[i],Cart));
+        for (let i = 0, len = products.length; i < len; i++) {
+            this.products.push(new ProductItem(products[i], Cart));
         }
     }
 
-    render(){
-        for (let i = 0,len = this.products.length; i < len; i++) {
+    render() {
+        for (let i = 0, len = this.products.length; i < len; i++) {
             this.DOM.append(this.products[i].card)
         }
     }
 }
 
 class ProductItem {
-    init(){
+    init() {
         this.id = undefined
         this.name = undefined
         this.desc = undefined
@@ -55,7 +57,8 @@ class ProductItem {
         this.img = undefined
         this.card = undefined
     }
-    constructor(product,Cart) {
+
+    constructor(product, Cart) {
         this.init()
         this.id = product.id
         this.name = product.name
@@ -63,53 +66,53 @@ class ProductItem {
         this.price = product.price
         this.curr = product.curr
         this.Cart = Cart
-        this.img = this.id+'.'+product.imgFormat;
+        this.img = this.id + '.' + product.imgFormat;
         this.card = this.createCard();
     }
 
-    createElement(type,classAdd) {
+    createElement(type, classAdd) {
         let elem = document.createElement(type)
         elem.classList.add(classAdd)
         return elem
     }
 
-    createCard(){
-        let card = this.createElement("div","card")
+    createCard() {
+        let card = this.createElement("div", "card")
 
-        let card_img = this.createElement("img","card-img")
-        card_img.src = './../img/'+this.img
+        let card_img = this.createElement("img", "card-img")
+        card_img.src = './../img/' + this.img
         card_img.alt = "image"
         card.appendChild(card_img)
 
-        let card_content = this.createElement("div","card-content")
+        let card_content = this.createElement("div", "card-content")
 
-        let card_title = this.createElement("div","card-title")
+        let card_title = this.createElement("div", "card-title")
         card_title.innerText = this.name
         card_content.appendChild(card_title)
 
-        let card_desc = this.createElement("div","card-desc")
+        let card_desc = this.createElement("div", "card-desc")
         card_desc.innerText = this.desc
         card_content.appendChild(card_desc)
 
         card.appendChild(card_content)
 
-        let card_footer = this.createElement("div","card-footer")
+        let card_footer = this.createElement("div", "card-footer")
 
-        let card_price = this.createElement("div","card-price")
+        let card_price = this.createElement("div", "card-price")
 
-        let card_price_value = this.createElement("div","card-price-value")
+        let card_price_value = this.createElement("div", "card-price-value")
         card_price_value.innerText = this.price
         card_price.appendChild(card_price_value)
 
-        let card_price_curr = this.createElement("div","card-price-curr")
+        let card_price_curr = this.createElement("div", "card-price-curr")
         card_price_curr.innerText = this.curr
         card_price.appendChild(card_price_curr)
 
         card_footer.appendChild(card_price)
 
-        let card_button = this.createElement("div","card-button")
-        card_button.innerText = "Buy"
-        card_button.addEventListener("click",(e) => this.buttonEvent())
+        let card_button = this.createElement("div", "card-button")
+        card_button.innerText = "Add t  o Cart"
+        card_button.addEventListener("click", (e) => this.buttonEvent())
 
         card_footer.appendChild(card_button)
 
@@ -118,70 +121,106 @@ class ProductItem {
         return card
     }
 
-    buttonEvent(){
+    buttonEvent() {
         this.Cart.addToCart(this)
     }
 }
 
 class Cart {
-    init(){
+    init() {
         this.DOM = undefined
         this.products = []
         this.total = undefined
     }
-    constructor(DOM,total) {
+
+    constructor(DOM, total) {
         this.init()
         this.DOM = DOM
         this.total = total
     }
 
-    render(){
+    render(products) {
+        let cart = this.getFromLocal()
+        for (let i = 0,len = cart.length; i < len; i++) {
+            for (let j = 0,p_len = products.length; j < p_len; j++) {
+                if (products[j].id == cart[i].id){
+                    this.addToCart(products[j],cart[i].count)
+                    break
+                }
+            }
+        }
 
     }
 
-    removeProduct(id){
+    getFromLocal() {
+        let cart = localStorage.getItem("cart")
+        if (cart === null) {
+            localStorage.setItem("cart", "[]")
+        }
+        return JSON.parse(localStorage.getItem("cart"))
+    }
+
+    addToLocal() {
+        let cart = [];
+        for (let i = 0,len = this.products.length; i < len; i++) {
+            cart.push({
+                id: this.products[i].id,
+                count: this.products[i].count
+            })
+        }
+        localStorage.setItem("cart",JSON.stringify(cart))
+    }
+
+    removeProduct(id) {
         this.products = this.products.filter(product => product.id !== id)
         this.calculateTotal()
     }
-    addToCart(product){
-        if (this.hasProducts(product.id)){
+
+    addToCart(product,count = 1) {
+        if (this.hasProducts(product.id)) {
             this.find(product.id).addCount()
         } else {
-            this.products.push(new CartItem(product,this))
+            this.products.push(new CartItem(product, this))
             this.DOM.append(this.find(product.id).card)
+            if (count != 1) {
+
+                this.find(product.id).addCount(count-1)
+            }
         }
         this.calculateTotal()
     }
 
-    hasProducts(id){
+    hasProducts(id) {
         let has = false
         if (this.find(id) != null)
             has = true
         return has
     }
 
-    find(id){
-        for (let i = 0,len = this.products.length; i < len; i++) {
-            if (id == this.products[i].id){
+    find(id) {
+        for (let i = 0, len = this.products.length; i < len; i++) {
+            if (id == this.products[i].id) {
                 return this.products[i]
             }
         }
         return null
     }
-    calculateTotal(){
+
+    calculateTotal() {
         let total = 0
-        for (let i = 0,len = this.products.length; i < len; i++) {
+        for (let i = 0, len = this.products.length; i < len; i++) {
             total += parseFloat(this.products[i].total)
         }
         if (Number(total) === total && total % 1 !== 0) {
             total = parseFloat(total).toFixed(2)
         }
         this.total.childNodes[1].children[1].innerText = total
+        this.addToLocal()
     }
 }
 
 class CartItem {
-    init(){
+    init() {
         this.id = undefined
         this.name = undefined
         this.count = 0
@@ -192,7 +231,8 @@ class CartItem {
         this.card = undefined
         this.Cart = undefined
     }
-    constructor(product,Cart) {
+
+    constructor(product, Cart) {
         this.init()
         this.id = product.id
         this.name = product.name
@@ -206,7 +246,8 @@ class CartItem {
         this.Cart.calculateTotal()
 
     }
-    calculateTotal(){
+
+    calculateTotal() {
         let total = this.count * this.price
         if (Number(this.price) === this.price && this.price % 1 !== 0) {
             this.total = parseFloat(total).toFixed(2)
@@ -214,84 +255,85 @@ class CartItem {
             this.total = total
         }
     }
-    addCount(count = 1){
+
+    addCount(count = 1) {
         this.count += count
-        if (this.count != 1){
+        if (this.count != 1) {
             this.countChange()
         }
     }
 
-    subtractCount(){
-        if (this.count != 1){
+    subtractCount() {
+        if (this.count != 1) {
             this.count--
             this.countChange()
         }
     }
 
-    countChange(){
+    countChange() {
         this.calculateTotal()
         this.card.childNodes[1].childNodes[1].childNodes[1].childNodes[1].innerText = this.count
         this.card.childNodes[2].childNodes[0].childNodes[1].childNodes[0].innerText = this.total
         this.Cart.calculateTotal()
     }
 
-    createElement(type,classAdd) {
+    createElement(type, classAdd) {
         let elem = document.createElement(type)
         elem.classList.add(classAdd)
         return elem
     }
 
-    createCard(){
-        let card = this.createElement("div","cart-card")
+    createCard() {
+        let card = this.createElement("div", "cart-card")
 
-        let card_img = this.createElement("img","cart-img")
+        let card_img = this.createElement("img", "cart-img")
         card_img.alt = "image"
-        card_img.src =  './../img/'+this.img
+        card_img.src = './../img/' + this.img
         card.appendChild(card_img)
 
-        let card_content = this.createElement("div","cart-content")
+        let card_content = this.createElement("div", "cart-content")
 
-        let card_title = this.createElement("div","cart-title")
+        let card_title = this.createElement("div", "cart-title")
         card_title.innerText = this.name
         card_content.appendChild(card_title)
 
-        let card_main = this.createElement("div","cart-main")
+        let card_main = this.createElement("div", "cart-main")
 
-        let card_price = this.createElement("div","cart-price")
+        let card_price = this.createElement("div", "cart-price")
 
-        let card_price_value = this.createElement("div","cart-price-value")
+        let card_price_value = this.createElement("div", "cart-price-value")
         card_price_value.innerText = this.price
         card_price.appendChild(card_price_value)
 
-        let card_price_curr = this.createElement("div","cart-price-curr")
+        let card_price_curr = this.createElement("div", "cart-price-curr")
         card_price_curr.innerText = this.curr
         card_price.appendChild(card_price_curr)
 
         card_main.appendChild(card_price)
 
-        let card_counter = this.createElement("div","cart-counter")
+        let card_counter = this.createElement("div", "cart-counter")
 
-        let subtract = this.createElement("div","subtract")
-        subtract.addEventListener("click",() => {
+        let subtract = this.createElement("div", "subtract")
+        subtract.addEventListener("click", () => {
             this.subtractCount()
         })
 
-        let icon_minus = this.createElement("i","fa-solid")
+        let icon_minus = this.createElement("i", "fa-solid")
         icon_minus.classList.add("fa-minus")
         subtract.appendChild(icon_minus)
 
         card_counter.appendChild(subtract)
 
-        let count = this.createElement("div","count")
+        let count = this.createElement("div", "count")
         count.innerText = this.count
         card_counter.appendChild(count)
 
-        let append = this.createElement("div","append")
-        append.addEventListener("click",() => {
+        let append = this.createElement("div", "append")
+        append.addEventListener("click", () => {
             this.addCount()
         })
 
-        let icon_plus = this.createElement("i","fa-solid")
+        let icon_plus = this.createElement("i", "fa-solid")
         icon_plus.classList.add("fa-plus")
         append.appendChild(icon_plus)
 
@@ -303,21 +345,21 @@ class CartItem {
 
         card.appendChild(card_content)
 
-        let card_aside = this.createElement("div","cart-aside")
+        let card_aside = this.createElement("div", "cart-aside")
 
-        let card_total = this.createElement("div","cart-total")
+        let card_total = this.createElement("div", "cart-total")
 
-        let card_total_title = this.createElement("div","cart-total-title")
+        let card_total_title = this.createElement("div", "cart-total-title")
         card_total_title.innerText = "total"
         card_total.appendChild(card_total_title)
 
-        let card_total_price = this.createElement("div","cart-total-price")
+        let card_total_price = this.createElement("div", "cart-total-price")
 
-        let total_value  = this.createElement("div","total-value")
+        let total_value = this.createElement("div", "total-value")
         total_value.innerText = this.total
         card_total_price.appendChild(total_value)
 
-        let total_curr  = this.createElement("div","total-curr")
+        let total_curr = this.createElement("div", "total-curr")
         total_curr.innerText = this.curr
         card_total_price.appendChild(total_curr)
 
@@ -325,14 +367,14 @@ class CartItem {
 
         card_aside.appendChild(card_total)
 
-        let card_remove  = this.createElement("div","cart-remove")
+        let card_remove = this.createElement("div", "cart-remove")
 
-        let remove_button  = this.createElement("div","remove-button")
-        remove_button.addEventListener("click",() => {
+        let remove_button = this.createElement("div", "remove-button")
+        remove_button.addEventListener("click", () => {
             this.removeProduct()
         })
 
-        let icon_trash = this.createElement("i","fa-solid")
+        let icon_trash = this.createElement("i", "fa-solid")
         icon_trash.classList.add("fa-trash")
 
         remove_button.appendChild(icon_trash)
@@ -346,12 +388,12 @@ class CartItem {
         return card
     }
 
-    removeProduct(){
+    removeProduct() {
         this.removeCard()
         this.Cart.removeProduct(this.id)
     }
 
-    removeCard(){
+    removeCard() {
         this.card.remove()
     }
 }
